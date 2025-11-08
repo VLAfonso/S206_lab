1c0ad2fd26d699c96a598f15e1a5378c9d21b8d2
Virgínia Letícia Afonso <vlafonso@hotmail.com>
2025-11-08 20:50:03 -0300

    test: add testes de criação de usuário e login

--- Exemplo/cypress/e2e/ExemploAula2.cy.js
+++ Exemplo/cypress/e2e/ExemploAula2.cy.js
@@ -1,5 +1,62 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference = cypress>
+
+describe("Testes da criação, registro e login", ()=>{
+  it("Teste criação de usuário com sucesso", ()=>{
+    criarUsuario()
+  })
+
+  it("Teste criação de usuário com falha", ()=>{
+    cy.visit('https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    cy.get('.btn-link').click()
+    cy.get('#firstName').type("Nome")
+    cy.get('#Text1').type("Sobrenome")
+    cy.get('#username').type("Usuário")
+
+    cy.get('.btn-primary').should("be.disabled")
+  })
+
+  it("Teste de login com sucesso", ()=>{
+    let infos = criarUsuario()
+    cy.visit('https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    login(infos)
+    cy.get('h1.ng-binding').should("contain.text", infos[0])
+  })
+
+  it("Teste de login com falha", ()=>{
+    let infos = criarUsuario()
+    cy.visit('https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+    login(infos)
+    cy.get('h1.ng-binding').should("contain.text", infos[0])
+    cy.get('.ng-binding > a').click()
+    cy.get('h1.ng-binding').should("not.contain.text", infos[0])
+    cy.get('.btn').click()
+    login(infos)
+    cy.get('.ng-binding').should("contain.text", "Username or password is incorrect")
   })
 })
+
+function criarUsuario(){
+  let hora = new Date().getHours().toString()
+  let minuto = new Date().getMinutes().toString()
+  let seg = new Date().getSeconds().toString()
+  let ID = hora + minuto + seg + "ID" 
+  let Senha = hora + minuto + seg + "Senha" 
+  let infos = [ID, Senha]
+
+  cy.visit('https://www.globalsqa.com/angularJs-protractor/registration-login-example/#/login')
+  cy.get('.btn-link').click()
+  cy.get('#firstName').type(ID)
+  cy.get('#Text1').type(ID)
+  cy.get('#username').type(ID)
+  cy.get('#password').type(Senha)
+  cy.get('.btn-primary').click()
+  cy.get('.ng-binding').should("contain.text", "Registration successful")
+
+  return infos
+}
+
+function login(infos){
+  cy.get('#username').type(infos[0])
+  cy.get('#password').type(infos[1])
+  cy.get('.btn-primary').click()
+}
